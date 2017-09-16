02309592c1a8d8bb2e42480774a5e766f30a3291
mxm <fjp790308448@gmail.com>
2017-09-16 21:18:58 +0800

    后台管理系统的 内容分类列表展示

---
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentCategoryServiceImpl.java
@@ -0,0 +1,42 @@
+package com.taotao.content.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.content.service.ContentCategoryService;
+import com.taotao.mapper.TbContentCategoryMapper;
+import com.taotao.pojo.TbContentCategory;
+import com.taotao.pojo.TbContentCategoryExample;
+import com.taotao.pojo.TbContentCategoryExample.Criteria;
+
+@Service
+public class ContentCategoryServiceImpl implements ContentCategoryService {
+
+	@Autowired
+	TbContentCategoryMapper contentCategoryMapper;
+	
+	@Override
+	public List<EasyUITreeNode> getContentCategoryList(long parentId) {
+		TbContentCategoryExample example = new TbContentCategoryExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andParentIdEqualTo(parentId);
+		
+		List<TbContentCategory> categories = contentCategoryMapper.selectByExample(example);
+		
+		List<EasyUITreeNode> nodeList = new ArrayList<>();
+		for (TbContentCategory category:categories) {
+			EasyUITreeNode node = new EasyUITreeNode();
+			node.setId(category.getId());
+			node.setText(category.getName());
+			node.setState(category.getIsParent()?"closed":"open");
+			
+			nodeList.add(node);
+		}
+		return nodeList;
+	}
+
+}
