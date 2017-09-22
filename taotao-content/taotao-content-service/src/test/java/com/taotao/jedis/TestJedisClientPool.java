301e0eb741d739234af644bc2289257dc6432e9b
mxm <fjp790308448@gmail.com>
2017-09-23 00:03:19 +0800

    jedisClient连接集群

--- taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedisClientPool.java
+++ taotao-content/taotao-content-service/src/test/java/com/taotao/jedis/TestJedisClientPool.java
@@ -1,19 +1,29 @@
 package com.taotao.jedis;
 
+import java.util.HashSet;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sun.tools.classfile.Opcode.Set;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
 
 public class TestJedisClientPool {
 
 	@Test
 	public void testJedisClientPool() {
+		
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-redis.xml");
 		
 		JedisClient jedisClient = applicationContext.getBean(JedisClient.class);
-		jedisClient.set("jedisClient", "test");
+		jedisClient.set("jedisClient", "test1");
 		
 		String result = jedisClient.get("jedisClient");
 		System.out.println(result);
+		
 	}
 }
