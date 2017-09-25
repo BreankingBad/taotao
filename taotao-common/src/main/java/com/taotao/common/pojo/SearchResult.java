245b7c7b28cf76b4eb5064807c091d45d0cf9001
mxm <fjp790308448@gmail.com>
2017-09-26 00:17:12 +0800

    搜索服务dao实现

--- taotao-common/src/main/java/com/taotao/common/pojo/SearchResult.java
+++ taotao-common/src/main/java/com/taotao/common/pojo/SearchResult.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class SearchResult implements Serializable{
 	private int totalPages;
+	private long recordCount;
 	private List<SearchItem> itemList;
 	public int getTotalPages() {
 		return totalPages;
@@ -12,12 +13,19 @@ public class SearchResult implements Serializable{
 	public void setTotalPages(int totalPages) {
 		this.totalPages = totalPages;
 	}
+	public long getRecordCount() {
+		return recordCount;
+	}
+	public void setRecordCount(long recordCount) {
+		this.recordCount = recordCount;
+	}
 	public List<SearchItem> getItemList() {
 		return itemList;
 	}
 	public void setItemList(List<SearchItem> itemList) {
 		this.itemList = itemList;
 	}
+	
 	
 	
 }
---
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/dao/SearchDao.java
@@ -0,0 +1,65 @@
+package com.taotao.search.dao;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import com.taotao.common.pojo.SearchItem;
+import com.taotao.common.pojo.SearchResult;
+
+@Repository
+public class SearchDao {
+
+	@Value("${SOLR_BASE_URL}")
+	private String SOLR_BASE_URL;
+	
+	public SearchResult search(SolrQuery solrQuery) throws Exception {
+		
+		SearchResult searchResult = new SearchResult();
+		
+		SolrClient client = new HttpSolrClient.Builder(SOLR_BASE_URL).build();
+		QueryResponse response = client.query(solrQuery);
+		
+		SolrDocumentList solrDocumentList = response.getResults();
+		searchResult.setRecordCount(solrDocumentList.getNumFound());
+		
+		List<SearchItem> itemList = new ArrayList<>();
+		for (SolrDocument solrDocument : solrDocumentList) {
+			SearchItem searchItem = new SearchItem();
+			
+			searchItem.setId((String) solrDocument.get("id"));
+			searchItem.setCategory_name((String) solrDocument.get("item_category_name"));
+			searchItem.setImage((String) solrDocument.get("item_image"));
+			searchItem.setItem_desc((String) solrDocument.get("item_desc"));
+			searchItem.setSell_point((String) solrDocument.get("item_sell_point"));
+			searchItem.setPrice((long) solrDocument.get("item_price"));
+			
+			Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
+			List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
+			if(list != null && list.size() > 0) {
+				// 如果标题有高亮显示，则取高亮显示的标题，否则取原来的标题
+				searchItem.setTitle(list.get(0));
+			}else {
+				searchItem.setTitle((String) solrDocument.get("item_title"));
+			}
+			
+			itemList.add(searchItem);
+		}
+		searchResult.setItemList(itemList);
+		
+		return searchResult;
+	}
+}
