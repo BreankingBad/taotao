b138547ee408de7a8a432d86d970af6b579f5ea1
mxm <fjp790308448@gmail.com>
2017-10-31 00:40:49 +0800

    商品详情页面添加redis缓存

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -10,7 +10,9 @@ import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,8 @@ import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.utils.IDUtils;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
@@ -42,10 +46,41 @@ public class ItemServiceImpl implements ItemService {
 	@Resource(name="itemAddTopic")
 	Destination destination;
 	
+	@Autowired
+	JedisClient jedisClient;
+	
+	@Value("${ITEM_INFO}")
+	String ITEM_INFO;
+	
+	@Value("${ITEM_EXPIRE_TIME}")
+	Integer ITEM_EXPIRE_TIME;
 	
 	@Override
 	public TbItem getItemById(long itemId) {
-		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		String redisKey = ITEM_INFO + ":" + itemId + ":BASE";
+		
+		try {
+			String itemInfo = jedisClient.get(redisKey);
+			if(StringUtils.isNoneBlank(itemInfo)) {
+				
+				TbItem item = JsonUtils.jsonToPojo(itemInfo, TbItem.class);
+				System.out.println("get item redis cache: "+itemId);
+				return item;
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		TbItem	item = itemMapper.selectByPrimaryKey(itemId);
+		
+		try {
+			jedisClient.set(redisKey, JsonUtils.objectToJson(item));
+			jedisClient.expire(redisKey, ITEM_EXPIRE_TIME);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return item;
 	}
 
@@ -100,7 +135,28 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public TbItemDesc getIemDescById(long itemId) {
+		String redisKey = ITEM_INFO + ":" + itemId + ":DESC";
+		
+		try {
+			String itemInfo = jedisClient.get(redisKey);
+			if(StringUtils.isNoneBlank(itemInfo)) {
+				TbItemDesc item = JsonUtils.jsonToPojo(itemInfo, TbItemDesc.class);
+				System.out.println("get itemdesc redis cache: "+itemId);
+				return item;
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
 		TbItemDesc tbItemDesc = itemDescMapper.selectByPrimaryKey(itemId);
+		
+		try {
+			jedisClient.set(redisKey, JsonUtils.objectToJson(tbItemDesc));
+			jedisClient.expire(redisKey, ITEM_EXPIRE_TIME);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return tbItemDesc;
 	}
 
