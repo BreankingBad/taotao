1bbcfb806eb07a812cb34f42c674d274799f65bb
mxm <fjp790308448@gmail.com>
2017-11-11 22:43:42 +0800

    检查数据是否可用 controller

--- taotao-sso/taotao-sso-service/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
+++ taotao-sso/taotao-sso-service/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package com.taotao.sso.service.impl;
 
-import static org.hamcrest.CoreMatchers.nullValue;
 
 import java.util.List;
 
-import org.apache.zookeeper.Op.Create;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
