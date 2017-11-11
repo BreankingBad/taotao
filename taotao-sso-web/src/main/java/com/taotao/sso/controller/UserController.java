e312969f5218a9eac5e522e1b6c49a5f08745267
mxm <fjp790308448@gmail.com>
2017-11-11 23:46:25 +0800

    用户注册接口实现

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
 
 @Controller
@@ -22,5 +23,13 @@ public class UserController {
 		TaotaoResult result = userService.checkData(param, type);
 		return result;
 	}
+	
+	@RequestMapping(value="/user/register",method=RequestMethod.POST)
+	@ResponseBody
+	public TaotaoResult register(TbUser tbUser) {
+		TaotaoResult taotaoResult = userService.register(tbUser);
+		return taotaoResult;
+	}
+	
 
 }
--- taotao-sso/taotao-sso-service/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
+++ taotao-sso/taotao-sso-service/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.taotao.sso.service.impl;
 
-
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.mapper.TbUserMapper;
@@ -18,31 +20,74 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	TbUserMapper tbUserMapper;
-	
+
 	@Override
 	public TaotaoResult checkData(String data, int type) {
-		
+
 		TbUserExample example = new TbUserExample();
 		Criteria criteria = example.createCriteria();
-		
+
 		// 检查用户名
-		if(type == 1) {
+		if (type == 1) {
 			criteria.andUsernameEqualTo(data);
-		// 检查手机号
-		}else if(type == 2) {
+			// 检查手机号
+		} else if (type == 2) {
 			criteria.andPhoneEqualTo(data);
-		// 检查邮箱
-		}else if(type == 3) {
+			// 检查邮箱
+		} else if (type == 3) {
 			criteria.andEmailEqualTo(data);
-		}else {
-			return TaotaoResult.build(400, "参数非法")	;
+		} else {
+			return TaotaoResult.build(400, "参数非法");
 		}
-		
+
 		List<TbUser> list = tbUserMapper.selectByExample(example);
-		if(list != null && !list.isEmpty()) {
+		if (list != null && !list.isEmpty()) {
 			return TaotaoResult.ok(false);
 		}
 		return TaotaoResult.ok(true);
+	}
+
+	@Override
+	public TaotaoResult register(TbUser user) {
+		// 检查数据的有效性
+		
+		if(StringUtils.isBlank(user.getUsername())) {
+			return TaotaoResult.build(400, "用户名不能为空");
+		}
+		
+		TaotaoResult nameResult = checkData(user.getUsername(), 1);
+		
+		if(!((boolean) nameResult.getData())) {
+			return TaotaoResult.build(400, "用户名重复");
+		}
+		
+		if(StringUtils.isBlank(user.getPassword())) {
+			return TaotaoResult.build(400, "密码不能为空");
+		}
+		
+		if (StringUtils.isNoneBlank(user.getPhone())) {
+			TaotaoResult phoneResult = checkData(user.getPhone(), 2);
+			if(!((boolean) phoneResult.getData())) {
+				return TaotaoResult.build(400, "电话号码重复");
+			}
+		}
+		
+		if (StringUtils.isNoneBlank(user.getEmail())) {
+			TaotaoResult emailResult = checkData(user.getEmail(), 3);
+			if(!((boolean) emailResult.getData())) {
+				return TaotaoResult.build(400, "邮箱重复");
+			}
+		}
+		
+		user.setCreated(new Date());
+		user.setUpdated(new Date());
+		
+		// 密码要md5加密
+		String md5Pwd = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+		user.setPassword(md5Pwd);
+		
+		tbUserMapper.insert(user);
+		return TaotaoResult.ok();
 	}
 
 }
