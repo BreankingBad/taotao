b1cd3ddc70d3fb6ca57ebedb92751985b6ebff4d
mxm <fjp790308448@gmail.com>
2017-11-19 15:15:46 +0800

    修改购物车商品数量逻辑

--- taotao-cart-web/src/main/java/com/taotao/cart/controller/CartController.java
+++ taotao-cart-web/src/main/java/com/taotao/cart/controller/CartController.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.utils.CookieUtils;
 import com.taotao.common.utils.JsonUtils;
 import com.taotao.pojo.TbItem;
@@ -67,5 +69,30 @@ public class CartController {
 		}
 		
 		return new ArrayList<>();
+	}
+	
+	@RequestMapping("/cart/cart")
+	public String showCartList(HttpServletRequest request) {
+		List<TbItem> itemList = getItemList(request);
+		request.setAttribute("cartList", itemList);
+		return "cart";
+	}
+	
+	@RequestMapping("/cart/update/num/{itemId}/{num}")
+	@ResponseBody
+	public TaotaoResult updateCartNum(@PathVariable long itemId,@PathVariable int num,HttpServletRequest request,
+			HttpServletResponse response) {
+		
+		List<TbItem> itemList = getItemList(request);
+		
+		for (TbItem tbItem : itemList) {
+			if(tbItem.getId() == itemId) {
+				tbItem.setNum(num);
+				break;
+			}
+		}
+		
+		CookieUtils.setCookie(request, response, CART_KEY, JsonUtils.objectToJson(itemList) , CART_EXPIRE , true);
+		return TaotaoResult.ok();
 	}
 }
